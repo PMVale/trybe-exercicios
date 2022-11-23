366a9d32cf34d77b03c364a5f41cd6ea6b98ff03
Pedro <pedrovale13@hotmail.com>
2022-11-23 16:18:15 -0300

    Faz o exercicio de variáveis

---
+++ fundamentos/secao-4-Introducao-a-JavaScript/dia-1-JavaScript-Primeiros-Passos/exercicio2/scripts.js
@@ -0,0 +1,15 @@
+const myName = 'Pedro';
+
+const birthCity = 'Rio de Janeiro';
+
+let birthYear = 1992;
+
+console.log(myName, birthCity, birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear)
+
+birthCity = 'Inferno'
+
+console.log(birthCity)
