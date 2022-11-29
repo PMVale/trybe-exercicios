580c91ca273c4e677f7441912fe53445bcc6d39b
Pedro <pedrovale13@hotmail.com>
2022-11-28 21:04:00 -0300

    Adiciona exercicio for

---
+++ fundamentos/secao-4-Introducao-a-JavaScript/dia-2-Javascript-Array-e-loop-For/exercicio for/for.js
@@ -0,0 +1,17 @@
+let number = 9;
+for (let index = 0; index <= 9; index++) {
+    console.log(number * index);
+}
+
+let listaDeNomes = ['Joana', 'Maria', 'Lucas'];
+
+for (i = 0; i < listaDeNomes.length; i++) {
+    let mensagem = 'Boas vindas, ' + listaDeNomes[i] + '!';
+    console.log(mensagem); 
+}
+
+let cars = ["Saab", "Volvo", "BMW"];
+
+for (let index = 0; index < cars.length; index += 1) {
+  console.log(cars[index]);
+}
