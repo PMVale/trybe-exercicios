16b9a6b368c12c3e93d12c389d99cbe44a3e2cba
Pedro <pedrovale13@hotmail.com>
2022-12-05 22:42:44 -0300

    termina exercicios

---
+++ fundamentos/secao-4-Introducao-a-JavaScript/dia-3-Javascript-Lógica-de-Programação/exercicioBonus2.js
@@ -0,0 +1,9 @@
+let number = 5;
+let image = '';
+let item = '*';
+let space = ' '; 
+
+for (let index = 0; index <= number; index += 1) {
+  console.log(image);
+  image += space[number] + item;
+}
